5b8f345325cc5b7dc47b1f64d7117fb1d74fff56
Saikat Bhadury <info2programmer@gmail.com>
2020-02-20 12:07:28 +0530

    User update api implement

--- application/controllers/api/Example.php
+++ application/controllers/api/Example.php
@@ -10,25 +10,18 @@ class Example extends RestController
 
   public function user_get($id = 0)
   {
-    // $users = array(
-    //   'userName' => 'Saikat Bhadury',
-    //   'id' => 1
-    // );
-
     // $this->response($users, RestController::HTTP_OK);
-    if($id > 0){
+    if ($id > 0) {
       // If userid exist then  this block will execute and send specific user
-      $userData = $this->common_model->find_data(['name' => 'users'],'row','',['id'=>$id]);
-      if($userData){
+      $userData = $this->common_model->find_data(['name' => 'users'], 'row', '', ['id' => $id]);
+      if ($userData) {
         $this->response($userData, RestController::HTTP_OK);
-      }
-      else{
+      } else {
         $this->response(['status' => false, 'msg' => "UserId not exist"], RestController::HTTP_NOT_FOUND);
       }
-    }
-    else{
+    } else {
       // If userid not exist then this block will execute and send all users
-      $userData = $this->common_model->find_data(['name' => 'users'],'array');
+      $userData = $this->common_model->find_data(['name' => 'users'], 'array');
       $this->response($userData, RestController::HTTP_OK);
     }
   }
@@ -36,22 +29,62 @@ class Example extends RestController
 
   public function user_delete($id)
   {
-    if($id){
-      $userData = $this->common_model->find_data(['name' => 'users'],'row','',['id'=>$id]);
-      if($userData){
-        if($this->common_model->delete_record('id',$id,'users')){
-          $this->response(['status' => true,'msg' => 'User deleted successfully'], RestController::HTTP_OK);
-        }
-        else{
+    if ($id) {
+      $userData = $this->common_model->find_data(['name' => 'users'], 'row', '', ['id' => $id]);
+      if ($userData) {
+        if ($this->common_model->delete_record('id', $id, 'users')) {
+          $this->response(['status' => true, 'msg' => 'User deleted successfully'], RestController::HTTP_OK);
+        } else {
           $this->response(['status' => false, 'msg' => "Some technical problem occured"], RestController::HTTP_NOT_MODIFIED);
         }
-      }
-      else{
+      } else {
         $this->response(['status' => false, 'msg' => "UserId not exist"], RestController::HTTP_NOT_FOUND);
       }
-    }
-    else{
+    } else {
       $this->response(['status' => false, 'msg' => "You are not allowed to delete the user"], RestController::HTTP_NOT_FOUND);
+    }
+  }
+
+  public function user_post()
+  {
+    $fullName = $this->input->post('fullName');
+    $email = $this->input->post('email');
+    $phoneNumber = $this->input->post('phoneNumber');
+    $dateOfBirth = $this->input->post('dateOfBirth');
+
+    if (!empty($fullName) && !empty($email) && !empty($phoneNumber) && !empty($dateOfBirth)) {
+      $data = array(
+        'fullName' => $fullName,
+        'email' => $email,
+        'phoneNumber' => $phoneNumber,
+        'dateOfBirth' => $dateOfBirth
+      );
+      $userId = $this->common_model->insert_record('users', $data);
+      $this->user_get($userId);
+    } else {
+      $this->response(['status' => false, 'msg' => "All Fields Mandetory"], RestController::HTTP_NOT_MODIFIED);
+    }
+  }
+
+
+  public function user_put()
+  {
+    $id = $this->put('id');
+    $fullName = $this->put('fullName');
+    $email = $this->put('email');
+    $phoneNumber = $this->put('phoneNumber');
+    $dateOfBirth = $this->put('dateOfBirth');
+    if (!empty($id) && !empty($fullName) && !empty($email) && !empty($phoneNumber) && !empty($dateOfBirth)) {
+      $data = array(
+        'fullName' => $fullName,
+        'email' => $email,
+        'phoneNumber' => $phoneNumber,
+        'dateOfBirth' => $dateOfBirth
+      );
+      $this->common_model->updateData($id, 'id',  $data, 'users');
+      $this->user_get($id);
+    } else {
+      $this->response(['status' => false, 'msg' => "All Fields Mandetory"], RestController::HTTP_NOT_MODIFIED);
     }
   }
 }
--- application/models/Common_model.php
+++ application/models/Common_model.php
@@ -1,204 +1,180 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 class Common_model extends CI_Model
 {
 	var $table;
-	
-    function  __construct(){
-        parent::__construct();
-    }
-    ################################ FETCH DATA ########################################
-    function getData($tableName,$searchByValue=null,$searchFields=null,$limit=null,$offset=null)
-    {
-        if($searchByValue!="" && $searchFields!=""){
-            $this->db->where($searchFields,$searchByValue);
-            $query = $this->db->get($tableName, $limit, $offset);
-            return $query->result_array();
-        }
-        else{
-            $query = $this->db->get($tableName, $limit, $offset);
-            return $query->result();
-        }
-        
-    }
-    ################################ UPDATE DATA ########################################
-	function updateData($searchByValue=null,$searchFields=null,$object,$tableName){
-        $this->db->where($searchFields, $searchByValue);
-        $this->db->update($tableName, $object);
-    }
-    
-    	// Delete Record
-	public function delete_record($where_feald,$delete_id,$table_name)
+
+	function  __construct()
+	{
+		parent::__construct();
+	}
+	################################ FETCH DATA ########################################
+
+	################################ UPDATE DATA ########################################
+	function updateData($searchByValue = null, $searchFields = null, $object, $tableName)
+	{
+		$this->db->where($searchFields, $searchByValue);
+		$this->db->update($tableName, $object);
+	}
+
+	// Delete Record
+	public function delete_record($where_feald, $delete_id, $table_name)
 	{
 		$this->db->where($where_feald, $delete_id);
 		$this->db->delete($table_name);
 		return true;
 	}
 	// Active Or Deactive Fealds
-	public function chage_status($where_feald,$update_id,$table_name,$object)
+	public function chage_status($where_feald, $update_id, $table_name, $object)
 	{
 		$this->db->where($where_feald, $update_id);
 		$this->db->update($table_name, $object);
-		
 	}
 	// Insert Function
-	public function insert_record($table_name,$object)
+	public function insert_record($table_name, $object)
 	{
 		$this->db->insert($table_name, $object);
 		$insert_id = $this->db->insert_id();
 		return  $insert_id;
 	}
 	// This function for image upload
-	public function upload_image($file,$path)
+	public function upload_image($file, $path)
 	{
-		
+
 		$config['upload_path'] = $path;
 		$config['allowed_types'] = 'gif|jpg|png|jpeg';
-		
-		
+
+
 		$this->load->library('upload', $config);
-		
-		if ( ! $this->upload->do_upload($file)){
+
+		if (!$this->upload->do_upload($file)) {
 			$error = array('error' => $this->upload->display_errors());
 			var_dump($error);
 			die;
-		}
-		else{
-			$fileData=$this->upload->data();
+		} else {
+			$fileData = $this->upload->data();
 		}
 		return $fileData['file_name'];
 	}
 	// This Function For Send SMTP mail
-	public function send_smtpmail($email,$subject,$mailBody)
+	public function send_smtpmail($email, $subject, $mailBody)
 	{
-		$config = Array(
-            'protocol' => 'smtp',
-            'smtp_host' => 'smtp.zoho.com',
-            'smtp_port' => 465,
-            'smtp_user' => 'admin@izifiso.com',
-            'smtp_pass' => '@BabaKiLand',
-            'mailtype'  => 'html', 
-            'charset'   => 'iso-8859-1'
-        );
+		$config = array(
+			'protocol' => 'smtp',
+			'smtp_host' => 'smtp.zoho.com',
+			'smtp_port' => 465,
+			'smtp_user' => 'admin@izifiso.com',
+			'smtp_pass' => '@BabaKiLand',
+			'mailtype'  => 'html',
+			'charset'   => 'iso-8859-1'
+		);
 		$this->load->library('email', $config);
-		
+
 		$this->email->from('admin@izifiso.com', 'IZIFISO ADMIN');
-        $this->email->to($email); 
-        $this->email->subject($subject);
-        $this->email->message($mailBody);  
-        $this->email->set_mailtype("html");
-        $this->email->send();
-    }
-    
-    public function checkcredential($username,$password){
+		$this->email->to($email);
+		$this->email->subject($subject);
+		$this->email->message($mailBody);
+		$this->email->set_mailtype("html");
+		$this->email->send();
+	}
+
+	public function checkcredential($username, $password)
+	{
 		//encrypt password
-		$password=md5($password);
-		$this->db->where('userName',$username);
-		$this->db->where('password',$password);
-        $result=$this->db->get('tbl_login');        
-		if($result->num_rows()==1){
+		$password = md5($password);
+		$this->db->where('userName', $username);
+		$this->db->where('password', $password);
+		$result = $this->db->get('tbl_login');
+		if ($result->num_rows() == 1) {
 			return $result->row(0)->id;
-		}
-		else{
+		} else {
 			return false;
 		}
 	}
-	
-	
+
+
 	// Find Data
-	public function find_data($table,$return_type='array',$list=NULL,$conditions='',$select='*',$join='',$group_by='',$order_by='',$limit=0,$offset=0,$or_where_in='',$or_like='')
+	public function find_data($table, $return_type = 'array', $list = NULL, $conditions = '', $select = '*', $join = '', $group_by = '', $order_by = '', $limit = 0, $offset = 0, $or_where_in = '', $or_like = '')
 	{
-		
-		$result = array();		
-		
-		$this->db->select($select);		
-		
-		if(!empty($table['alias_name']))
-		{
-			$table_name = $table['name'].' as '.$table['alias_name'];
-		}
-		else
-		{
+
+		$result = array();
+
+		$this->db->select($select);
+
+		if (!empty($table['alias_name'])) {
+			$table_name = $table['name'] . ' as ' . $table['alias_name'];
+		} else {
 			$table_name = $table['name'];
 		}
 		$this->db->from($table_name);
-		
-		if(is_array($join))
-		{
-			for($j=0;$j<count($join);$j++)
-			{
-				if($join[$j]['table'])
-				{
-					/*$table_join = $join[$j]['table'].' as '.$join[$j]['table_alias']*/;
+
+		if (is_array($join)) {
+			for ($j = 0; $j < count($join); $j++) {
+				if ($join[$j]['table']) {
+						/*$table_join = $join[$j]['table'].' as '.$join[$j]['table_alias']*/;
 					//$table_join_name = $join[$j]['table_alias'];
 					$table_join = $join[$j]['table'];
 					$table_join_name = $join[$j]['table'];
-				}
-				else
-				{
+				} else {
 					/*$table_join = $join[$j]['table'];
 					$table_join_name = $join[$j]['table'];*/
 				}
-				if(!empty($join[$j]['table_master_alias']))
-				{
+				if (!empty($join[$j]['table_master_alias'])) {
 					$table_master_join = $join[$j]['table_master_alias'];
-				}
-				else
-				{
+				} else {
 					$table_master_join = $join[$j]['table_master'];
 				}
-				$this->db->join($table_join,$table_join_name.'.'.$join[$j]['field'].'='.$table_master_join.'.'.$join[$j]['field_table_master']/*.$join[$j]['and']*/,$join[$j]['type']);
+				$this->db->join($table_join, $table_join_name . '.' . $join[$j]['field'] . '=' . $table_master_join . '.' . $join[$j]['field_table_master']/*.$join[$j]['and']*/, $join[$j]['type']);
 			}
 		}
-		
-		if($conditions != '')$this->db->where($conditions);	
-		if($or_where_in != '')$this->db->or_where_in($or_where_in);
-		
-		if($or_like != '')$this->db->or_like($or_like);		
-		
-		if(is_array($group_by))
-		{
-			for($g=0;$g<count($group_by);$g++)
-			{
+
+		if ($conditions != '') $this->db->where($conditions);
+		if ($or_where_in != '') $this->db->or_where_in($or_where_in);
+
+		if ($or_like != '') $this->db->or_like($or_like);
+
+		if (is_array($group_by)) {
+			for ($g = 0; $g < count($group_by); $g++) {
 				$this->db->group_by($group_by[$g]);
 			}
 		}
-		
-		if(is_array($order_by))
-		{
-			for($o=0;$o<count($order_by);$o++)
-			{
-				$this->db->order_by($order_by[$o]['field'],$order_by[$o]['type']);
+
+		if (is_array($order_by)) {
+			for ($o = 0; $o < count($order_by); $o++) {
+				$this->db->order_by($order_by[$o]['field'], $order_by[$o]['type']);
 			}
 		}
-		
-		if($limit != 0)$this->db->limit($limit,$offset);
-		
-		
+
+		if ($limit != 0) $this->db->limit($limit, $offset);
+
+
 		$query = $this->db->get();
-		
+
 		//echo $this->db->last_query();
-		switch ($return_type) 
-		{
+		switch ($return_type) {
 			case 'array':
 			case '':
-				if($query->num_rows() > 0){$result = $query->result();}
+				if ($query->num_rows() > 0) {
+					$result = $query->result();
+				}
 				break;
-				
+
 			case 'row':
-				if($query->num_rows() > 0){$result = $query->row();}
+				if ($query->num_rows() > 0) {
+					$result = $query->row();
+				}
 				break;
-			
+
 			case 'row-array':
-				if($query->num_rows() > 0){$result = $query->row_array();}
+				if ($query->num_rows() > 0) {
+					$result = $query->row_array();
+				}
 				break;
-				
+
 			case 'list':
-			
-				$list_arr[''] = 'Choose'. $list['empty_name'];
-				if($query->num_rows() > 0)
-				{
-					foreach ($query->result() as $row)
-					{
+
+				$list_arr[''] = 'Choose' . $list['empty_name'];
+				if ($query->num_rows() > 0) {
+					foreach ($query->result() as $row) {
 						$list_arr[$row->$list['key']] = $row->$list['value'];
 					}
 				} else {
@@ -206,13 +182,13 @@ class Common_model extends CI_Model
 				}
 				$result = $list_arr;
 				break;
-				
+
 			case 'count':
 				$result = $query->num_rows();
 				break;
 		}
 		//echo $this->db->last_query();die;
-        return $result;
+		return $result;
 	}
 
 	// This Fuction For Get Recent Blog Data 
@@ -240,22 +216,23 @@ class Common_model extends CI_Model
 		$recentBlogQuery = $this->db->get();
 		echo $this->db->last_query();
 		die;
-		return 	$recentBlogQuery->result();	
+		return 	$recentBlogQuery->result();
 	}
 
 
 	// This Function For Remove All Special Char
-	public function cleanSpecialChar($string) {
+	public function cleanSpecialChar($string)
+	{
 		$string = trim($string);
 		$string = strtolower($string);
 		$string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
-	 
+
 		return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
 	}
 
 
 	// This Section For Similar Product
-	public function count($where,$tableName)
+	public function count($where, $tableName)
 	{
 		$this->db->where($where);
 		$query = $this->db->get($tableName);
@@ -266,8 +243,8 @@ class Common_model extends CI_Model
 
 
 	// This Section For Get Seat Accomodation
-	public function getSeatAccomodatiob($stay,$toDate,$fromDate,$productId,$productType)
-	{	
+	public function getSeatAccomodatiob($stay, $toDate, $fromDate, $productId, $productType)
+	{
 		$this->db->where('seat_id', $stay);
 		$this->db->where('date >=', $toDate);
 		$this->db->where('date <=', $fromDate);
@@ -275,14 +252,7 @@ class Common_model extends CI_Model
 		$this->db->select('SUM(seat) as totalSeat,max_head,date');
 		$this->db->from('tbl_book');
 		$this->db->join('seat_accomodation', 'seat_accomodation.id = tbl_book.seat_id', 'INNER');
-		$query=$this->db->get();
+		$query = $this->db->get();
 		return $query->result();
-		
 	}
-
-	
-
-	
-    
-	
 }
